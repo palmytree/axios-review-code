f421d2548dda868d5e79002727198d47c30f4d2a
mattcbodily <mattcbodily@gmail.com>
2020-05-18 08:32:09 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Form from './Components/Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+  constructor(props){
+    super(props);
+    this.state = {
+      vehicles: []
+    }
+  }
+
+  render(){
+    console.log(this.state.vehicles)
+    return (
+      <div className="App">
+        WR1 HTTP/Axios Review
+        <Form />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+export default class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            make: '',
+            model: '',
+            year: 0,
+            color: '',
+            price: 0
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    render(){
+        const {make, model, year, color, price} = this.state
+        return(
+            <div>
+                <input name='make' value={make} onChange={e => this.handleChange(e)} placeholder='make'/>
+                <input name='model' value={model} onChange={e => this.handleChange(e)} placeholder='model'/>
+                <input name='year' value={year} onChange={e => this.handleChange(e)} placeholder='year' type='number'/>
+                <input name='color' value={color} onChange={e => this.handleChange(e)} placeholder='color'/>
+                <input name='price' value={price} onChange={e => this.handleChange(e)} placeholder='price' type='number'/>
+                <button onClick={this.addNewCar}>Add Vehicle</button>
+            </div>
+        )
+    }
+}
