86692fbd4b255530743caaa92d3267d30b6ca0e8
mattcbodily <mattcbodily@gmail.com>
2020-05-18 08:42:15 -0600

    added VehicleDisplay component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Form from './Components/Form';
+import VehicleDisplay from './Components/VehicleDisplay';
 import './App.css';
 
 class App extends Component  {
@@ -12,10 +13,14 @@ class App extends Component  {
 
   render(){
     console.log(this.state.vehicles)
+    const mappedVehicles = this.state.vehicles.map((vehicle, i) => (
+      <VehicleDisplay key={i} vehicle={vehicle}/>
+    ))
     return (
       <div className="App">
         WR1 HTTP/Axios Review
         <Form />
+        {mappedVehicles}
       </div>
     )
   }
---
+++ src/Components/VehicleDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VehicleDisplay = props => (
+    <div>
+        
+    </div>
+);
+
+export default VehicleDisplay;
